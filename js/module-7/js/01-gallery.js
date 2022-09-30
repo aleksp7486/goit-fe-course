61a80bbf1d81a65a0e89ef4e77a975d58658f011
aleksp7468 <aleksandrpopov7486@gmail.com>
2022-09-30 19:46:21 +0300

    js-m7-task-1

--- js/module-7/js/01-gallery.js
+++ js/module-7/js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const items = createElements(galleryItems);
+gallery.append(...items);
+gallery.addEventListener('click', showOriginalImage);
+
+function createElements(arr) {
+  return arr.map(elem => {
+    const { preview, original, description } = elem;
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add('gallery__item');
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+    galleryLink.setAttribute('href', original);
+    galleryItem.append(galleryLink);
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.setAttribute('src', preview);
+    galleryImage.setAttribute('data-source', original);
+    galleryImage.setAttribute('alt', description);
+    galleryImage.style.maxWidth = '800px';
+    galleryLink.append(galleryImage);
+    return galleryItem;
+  });
+}
+
+function showOriginalImage(evt) {
+  evt.preventDefault();
+  const image = evt.target;
+  if (image.nodeName !== 'IMG') {
+    return;
+  }
+  image.setAttribute('src', image.dataset.source);
+  const instance = basicLightbox.create(`${image.outerHTML}`, {
+    onShow: () => window.addEventListener('keydown', handlePressKey),
+    onClose: () => window.removeEventListener('keydown', handlePressKey),
+  });
+
+  function handlePressKey(evt) {
+    if (evt.code === 'Escape') {
+      instance.close();
+    }
+  }
+  instance.show();
+}
