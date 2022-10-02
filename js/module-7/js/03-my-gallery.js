09f231b40a37ddf450cdb74c4cb002b3312043cd
aleksp7468 <aleksandrpopov7486@gmail.com>
2022-10-02 16:51:38 +0300

    js-m7-my-gallery_add-observe

--- js/module-7/js/03-my-gallery.js
+++ js/module-7/js/03-my-gallery.js
@@ -60,3 +60,31 @@ function showOriginalImage(evt) {
     link.classList.remove('modal');
   }
 }
+
+// Section observe
+
+const onEntry = entries => {
+  entries.forEach(entries => {
+    if (entries.isIntersecting) {
+      const entriesSectionId = entries.target.getAttribute('id');
+      const activeLink = document.querySelector('.nav__link--active');
+      if (activeLink) {
+        activeLink.classList.remove('nav__link--active');
+      }
+      const nextLink = document.querySelector(`a[href='#${entriesSectionId}']`);
+      nextLink.classList.add('nav__link--active');
+    }
+  });
+};
+
+const options = {
+  threshold: 0.5,
+};
+
+const sections = document.querySelectorAll('.js-section');
+
+const observer = new IntersectionObserver(onEntry, options);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
